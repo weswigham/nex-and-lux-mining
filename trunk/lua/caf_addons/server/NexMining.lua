4e8f4eb22e5b31fb7c2309591248e48d8bfc5247
levybreak098 <levybreak098@0d34374c-d9e1-11dd-9d6a-15002d8776aa>
2009-01-03 22:14:49 +0000

    Basic Structure

---
+++ trunk/lua/caf_addons/server/NexMining.lua
@@ -0,0 +1,66 @@
+--[[ Serverside Custom Addon file Base ]]--
+
+local RD = {}
+
+/*
+	The Constructor for this Custom Addon Class
+*/
+function RD.__Construct()
+	if status then return false, "Already Active!" end
+	if not CAF.GetAddon("Resource Distribution") or not CAF.GetAddon("Resource Distribution").GetStatus() then return false, "Resource Distribution is Required and needs to be Active!" end
+		return true , "Nex Mining Activated"
+end
+
+/**
+	The Destructor for this Custom Addon Class
+*/
+function RD.__Destruct()
+	if not status then return false, "Addon is already disabled!" end
+	status = false
+	return true
+end
+
+/**
+	Get the required Addons for this Addon Class
+*/
+function RD.GetRequiredAddons()
+	return {"Resource Distribution"}
+end
+
+/**
+	Get the Boolean Status from this Addon Class
+*/
+function RD.GetStatus()
+	return status
+end
+
+/**
+	Get the Version of this Custom Addon Class
+*/
+function RD.GetVersion()
+	return 0.1, "Alpha"
+end
+
+/**
+	Get any custom options this Custom Addon Class might have
+*/
+function RD.GetExtraOptions()
+	return {}
+end
+
+/**
+	Get the Custom String Status from this Addon Class
+*/
+function RD.GetCustomStatus()
+	return "Not Implemented Yet"
+end
+
+/**
+	You can send all the files from here that you want to add to send to the client
+*/
+function RD.AddResourcesToSend()
+	
+end
+
+CAF.RegisterAddon("Nex Mining", RD, "2") 
+
