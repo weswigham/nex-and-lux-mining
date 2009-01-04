55dd6494c498a6127cc80621446a5d3a2de50f89
levybreak098 <levybreak098@0d34374c-d9e1-11dd-9d6a-15002d8776aa>
2009-01-04 01:33:53 +0000

    grah.

--- trunk/lua/entities/nex_resource_storage/cl_init.lua
+++ trunk/lua/entities/nex_resource_storage/cl_init.lua
@@ -1,3 +1,7 @@
+<<<<<<< .mine
 include('shared.lua')
 
-language.Add("generator_gas_co2", "Carbon Dioxide Generator")
+language.Add("nex_resource_storage", "Nex Resource Storage")=======
+include('shared.lua')
+
+language.Add("generator_gas_co2", "Carbon Dioxide Generator")>>>>>>> .r6
--- trunk/lua/entities/nex_resource_storage/shared.lua
+++ trunk/lua/entities/nex_resource_storage/shared.lua
@@ -1,5 +1,13 @@
+<<<<<<< .mine
+ENT.Type 		= "anim"
+ENT.Base 		= "base_rd_entity"
+ENT.PrintName 	= "Resource Storage"
+
+list.Set( "LSEntOverlayText" , "nex_resource_storage", {HasOOO = false, num=-1} )
+=======
 ENT.Type 		= "anim"
 ENT.Base 		= "base_rd_entity"
 ENT.PrintName 	= "Resource Storage"
 
 list.Set( "LSEntOverlayText" , "resource_storage", {HasOOO = false, num=-1} )
+>>>>>>> .r6
